d21de08cec6d7d14ab149f9727bf27d59ed7643e
AlexDateling <ADateling@Gmail.com>
2024-09-06 00:29:03 +0200

    created workflow for bank to create a transaction

--- api/main.go
+++ api/main.go
@@ -1,95 +1,113 @@
 package main
 
 import (
+	"crypto/rand"
 	"example/web-service-gin/docs"
 	"fmt"
+	"math/big"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin" // swagger embed files
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
 )
 
+// Bank Mock Code of client Data //
 type Client struct {
-	ID         string  `json:"id"`
-	Name       string  `json:"name"`
-	Surname    string  `json:"surname"`
-	BankNumber string  `json:"banknumber"`
-	BankID     string  `json:"bankid"`
-	Balance    float64 `json:"balance"`
+	ID            string  `json:"id"`
+	Name          string  `json:"name"`
+	Surname       string  `json:"surname"`
+	AccountNumber string  `json:"accountnumber"`
+	BankID        string  `json:"bankid"`
+	Balance       float64 `json:"balance"`
 }
-
 type Bank struct {
-	ID      string `json:"id"`
+	BankID  string `json:"bankid"`
 	Name    string `json:"name"`
 	Country string `json:"country"`
 }
 
+///////////////////////////////////
+
 // send this to smart contract
 type transaction struct {
-	ID             string  `json:"id"`
-	ClientSend     Client  `json:"clientsend"`
-	ClientReceive  Client  `json:"clientreceive"`
-	RequestingBank Bank    `json:"requestingbank"`
-	BankOwed       Bank    `json:"bankowed"`
-	AmountOwed     float64 `json:"amount"`
+	TransactionID   string         `json:"transaction_id"`
+	ClientDetails   AccountDetails `json:"client_details"`
+	ReceiverDetails AccountDetails `json:"receiver_details"`
+
+	Amount float64 `json:"amount"`
 }
 
-// albums slice to seed record album data.
+type AccountDetails struct {
+	Name          string `json:"name"`
+	Surname       string `json:"surname"`
+	AccountNumber string `json:"accountnumber"`
+	BankDetails   Bank   `json:"bankdetails"`
+}
+
+///////////////////////////////////
+
+// payload used to start a new transaction
+type payload struct {
+	Amount          float64        `json:"amount"`
+	ReceiverDetails AccountDetails `json:"receiverdetails"`
+}
+
+///////////////////////////////////
+
+// MockData
 var Sender = Client{
-	ID:         "alexsid",
-	Name:       "Alex",
-	Surname:    "Dateling",
-	BankNumber: "0000000000",
-	BankID:     "ABSA645334",
-	Balance:    4000.00}
+	ID:            "alexsid",
+	Name:          "Alex",
+	Surname:       "Dateling",
+	AccountNumber: "0000000000",
+	BankID:        "ABSA645334",
+	Balance:       4000.00}
 
 var SenderBank = Bank{
-	ID:      "ABSA645334",
+	BankID:  "ABSA645334",
 	Name:    "ABSA",
 	Country: "ZAR"}
 
-var Receiver = Client{
-	ID: "mumumumumumu",
-	Name:       "T",
-	Surname:    "Kok",
-	BankNumber: "11111111111",
-	BankID:     "FNB20003",
-	Balance:    6.00}
+///////////////////////////////////
 
-// getAlbums responds with the list of all albums as JSON.
-func createPayment(c *gin.Context) {
-	clientid := c.Param("clientid")
+/////////////////////////////////// FUNCTIONS
 
-	var newTransaction transaction
+func generateTransactionReference(clientID string, receiverAccountNumber string) (string, error) {
+	// Get the current timestamp in milliseconds
+	timestamp := time.Now().UnixNano() / int64(time.Millisecond)
 
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	// Bind the received JSON to newTransaction
-	if err := c.BindJSON(&newTransaction); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
+	// Generate a random number
+	randomNumber, err := rand.Int(rand.Reader, big.NewInt(1000000)) // up to 999999
+	if err != nil {
+		return "", err
 	}
 
-	loginClient := getClient(clientid)
-	clientBank := getBank(loginClient.BankID)
+	// Format the reference number as a string
+	reference := fmt.Sprintf("%d%s%s%06d", timestamp, clientID, receiverAccountNumber, randomNumber.Int64())
 
-	// fmt.Println(clientBank)
+	return reference, nil
+}
 
-	newTransaction.ClientSend = loginClient
-	newTransaction.RequestingBank = clientBank
-
-	var isEnough bool = checkBalance(newTransaction.ClientSend, newTransaction.AmountOwed)
-
-	// Add the new album to the slice.
-	if isEnough {
-		fmt.Println(newTransaction)
-		c.IndentedJSON(http.StatusCreated, newTransaction)
+// Function to check the balance of the client to see if there is enough funds to proceed with the transaction
+func checkBalance(AccountHolder Client, amount float64) (result bool) {
+	if AccountHolder.Balance < amount {
+		fmt.Println("no enough munney")
+		return false
+	} else {
+		return true
 	}
+}
+
+// send transaction to chaincode
+func sendtoChaincode(newTransaction transaction) (result bool) {
 
 }
 
-// getAlbums responds with the list of all albums as JSON.
+/////////////////////////////////// MOCK DATA FUNCTIONS
+
+// Function to get the mockdata of the client
 func getClient(clientId string) (result Client) {
 	if clientId == Sender.ID {
 		fmt.Println("yay")
@@ -100,8 +118,9 @@ func getClient(clientId string) (result Client) {
 	}
 }
 
+// Function to get the mockdata of the client's bank
 func getBank(bankId string) (result Bank) {
-	if bankId == SenderBank.ID {
+	if bankId == SenderBank.BankID {
 		fmt.Println("yay")
 		return SenderBank
 	} else {
@@ -110,18 +129,70 @@ func getBank(bankId string) (result Bank) {
 	}
 }
 
-// getAlbums responds with the list of all albums as JSON.
-func checkBalance(AccountHolder Client, amount float64) (result bool) {
-	if AccountHolder.Balance < amount {
-		fmt.Println("no enough munney")
-		return false
+/////////////////////////////////// API Calls
+
+func createPayment(c *gin.Context) {
+	clientid := c.Param("clientid")
+
+	var newPayload payload
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	// Bind the received JSON to newTransaction
+	if err := c.BindJSON(&newPayload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	fmt.Println(newPayload)
+
+	//validate payload
+
+	// create transaction json from payload and client stuff
+
+	var transaction_id string
+
+	loginClient := getClient(clientid)
+	clientBank := getBank(loginClient.BankID)
+
+	fmt.Println(clientBank)
+
+	// Need to generate a proper reference
+	transaction_id, err := generateTransactionReference(loginClient.ID, newPayload.ReceiverDetails.AccountNumber)
+	if err != nil {
+		fmt.Println("Error generating reference number:", err)
+		return
+	}
+
+	fmt.Println(transaction_id)
+
+	var newTransaction transaction
+
+	var isEnough bool = checkBalance(loginClient, newPayload.Amount)
+
+	if isEnough {
+		newTransaction.TransactionID = transaction_id
+		newTransaction.ClientDetails = AccountDetails{
+			Name:          loginClient.Name,
+			Surname:       loginClient.Surname,
+			AccountNumber: loginClient.AccountNumber,
+			BankDetails:   clientBank,
+		}
+		newTransaction.ReceiverDetails = newPayload.ReceiverDetails
+		newTransaction.Amount = newPayload.Amount
+
+		fmt.Println("XXXXXXXXXXXXXX")
+		fmt.Println(newTransaction)
+
+		sendtoChaincode()
+		c.IndentedJSON(http.StatusCreated, newTransaction)
 	} else {
-		return true
+		c.IndentedJSON(http.StatusBadRequest, `{"status": "failed"}`)
 	}
 }
 
-// // getAlbums responds with the list of all albums as JSON.
-// func getTimeStamp(c *gin.Context) {
+// Send to chaincode
+// func sendLedger(c *gin.Context) {
 // 	c.IndentedJSON(http.StatusOK, albums)
 // }
 
